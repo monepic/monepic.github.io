55a3f1a538f836e86f19e2f365a6e5de6b5e28dc
Ed <ed@monepic.com>
2018-10-21 22:28:07 +0100

    v1

---
+++ precache-manifest.5b30efaf5293e16022d28eec503b440d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e8cfa404f815afa4ee9",
+    "url": "monepic.github.io/static/js/runtime~main.3e8cfa40.js"
+  },
+  {
+    "revision": "51bc15065cc25d6b9ff3",
+    "url": "monepic.github.io/static/js/main.51bc1506.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "monepic.github.io/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "51bc15065cc25d6b9ff3",
+    "url": "monepic.github.io/static/css/main.2408bc4a.chunk.css"
+  },
+  {
+    "revision": "d5604f3795da7ece6803ac6a44aa157d",
+    "url": "monepic.github.io/index.html"
+  }
+];
